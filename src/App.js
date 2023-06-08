0284b6d9b81f3dd25b6befd023a85a4374852ee4
Jungmin Moon <jm.moon0801@gmail.com>
2023-06-08 15:01:10 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from "./logo.svg";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Main from "./routes/main"
 import "./App.css";
+
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 
 function App() {
@@ -7,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="" element={<Front/>} />
-        <Route path="/main" element={<div>공사중</div>} />
+        <Route path="" element={ <Front/> } />
+        <Route path="/main" element={ <Main/> } />
       </Routes>
     </div>
   );
@@ -23,21 +28,21 @@ function Front() {
       <div className="front-img">
         <div className="front-bg"></div>
         <h2>냉장고 파먹기</h2>
-        <div>
-          <div>
-            <p>냉장고 속 재료들로</p>
-            <p>무엇을 해먹을 수 있을까?</p>
-          </div>
-          <div>
-            <p> 이제 냉장고를 열고 </p>
-            <p>무엇을 해먹을지</p>
-            <p>고민하지 않으셔도 됩니다.</p>
-          </div>
-          <div>
-            <p>요리가 어려운 당신을 위해 </p>
-            <p>냉장고 속 식재료들에 맞는</p>
-            <p>레시피를 추천해드립니다.</p>
-          </div>
+        <div className="front-description">
+          <p>
+            <span>냉장고 속 재료들로</span>
+            <span>무엇을 해먹을 수 있을까?</span>
+          </p>
+          <p>
+            <span> 이제 냉장고를 열고 </span>
+            <span>무엇을 해먹을지</span>
+            <span>고민하지 않으셔도 됩니다.</span>
+          </p>
+          <p>
+            <span>요리가 어려운 당신을 위해 </span>
+            <span>냉장고 속 식재료들에 맞는</span>
+            <span>레시피를 추천해드립니다.</span>
+          </p>
         </div>
         <button onClick={ ()=>{navigate('/main')} }>시작하기</button>
       </div>
---
+++ src/routes/main.js
@@ -0,0 +1,248 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faPlus, faXmark, faMagnifyingGlass, faUtensils, faFire} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import "./main.css";
+import axios from 'axios'
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
+
+
+function Main() {
+  let [ingredients, setIngredients] = useState([]);
+  let [input, setInput] = useState("");
+  let [results, setResults] = useState([]);
+  let [recipe, setRecipe] = useState([]);
+
+
+  const callAPI = () =>{
+    let apikey = '20cb511a2c2044259d0e';
+          axios.get(`http://openapi.foodsafetykorea.go.kr/api/${apikey}/COOKRCP01/json/1/10`)
+          .then((result)=>{
+            let data = result.data.COOKRCP01.row
+            setResults(data);
+          })
+          .catch(()=>{
+            console.log('Fail')
+          })   
+  };
+
+
+  const findRecipe = () => {
+    let copy = [...recipe];
+
+    for(let i=0; i<ingredients.length; i++){
+      let filtered = results.filter((value)=>
+        value.RCP_PARTS_DTLS.includes(ingredients[i])
+      )
+      copy.push(...filtered);
+      setRecipe(copy);
+    }
+    };
+
+  useEffect(()=>{
+    findRecipe()
+  },[results])
+
+  const addIngredients = () => {
+    if (ingredients.length > 4) {
+      alert("최대 5개까지만 입력 가능합니다.");
+    } else {
+      let copy = [...ingredients];
+      copy.push(input);
+      setIngredients(copy);
+      setInput('');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addIngredients()
+    }
+  };
+
+
+
+
+  return (
+    <main className="main">
+      <article>
+        <Navbar>
+          <Container>
+            <Navbar.Brand href="#home" className="brand">
+              냉장고 파먹기
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <FontAwesomeIcon icon={faBars} />
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <section>
+          <div className="search-container">
+            <div className="search-area">
+              <div className="input-area">
+                <input
+                  type="text"
+                  placeholder="재료를 입력하세요"
+                  onChange={(e) => {
+                    setInput(e.target.value);
+                  }}
+                  onKeyDown={handleKeyPress}
+                  value={ input }
+                ></input>
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className="search-icon"
+                  onClick={ addIngredients }
+                />
+              </div>
+              <div className="ingredients-area">
+                {ingredients.map((item, i) => {
+                  return (
+                    <span className="ingredients-box" key={i}>
+                      <span>{ingredients[i]}</span>
+                      <FontAwesomeIcon icon={faXmark} className="delete-icon" onClick={()=>{
+                        let copy = [...ingredients];
+                        copy.splice(i,1);
+                        setIngredients(copy);
+                      }}/>
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+            {
+              Object.keys(results).length > 0? <Results results={results} recipe={recipe} /> : 
+              ingredients.length > 0 ? <After callAPI={()=>{
+                callAPI()
+              }}/> : <Before/> 
+            }
+
+        </section>
+      </article>
+    </main>
+  );
+}
+
+
+
+function Before() {
+  return (
+    <div className="result-area" >
+      <div className="result-container">
+        <div className="face-icon">🙄</div>
+        <div style={{fontWeight:'bold'}}>먼저 식재료를 추가해주세요</div>
+      </div>
+      </div>
+  );
+}
+
+function After(props){
+  return (
+    <div className="result-area" >
+      <div className="result-container">
+        <div className="face-icon">😋</div>
+        <div style={{fontWeight:'bold'}}>이제 레시피를 찾아볼까요?</div>
+        <button onClick={()=>{
+          props.callAPI()
+        }}
+        >Search</button>
+      </div>
+      </div>
+  );
+}
+
+function Results(props) {
+
+  const shortenWords = (str, length = 90) => {
+    let description = '';
+    if (str.length > length) {
+      description = str.substr(0, length - 2) + '...';
+    } else {
+      description = str;
+    }
+    return description;
+  };
+
+
+  return (
+    <div className="render-recipe">
+      {props.recipe.map((item, i) => {
+        return (
+          <Container className="recipe-container" key={i}>
+            <Row style={{ padding: "5px 0" }}>
+              <Col sm={4} className="recipe-box1">
+                <img
+                  src={props.recipe[i].ATT_FILE_NO_MAIN}
+                  className="result-image"
+                />
+              </Col>
+              <Col sm={8} className="recipe-box2">
+                <h3 className="recipe-title">{props.results[i].RCP_NM}</h3>
+                <p className="recipe-details">
+                  {shortenWords(props.recipe[i].RCP_PARTS_DTLS)}
+                </p>
+                <p className="recipe-etc">
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faUtensils}
+                      style={{ marginRight: "6px" }}
+                    />
+                    {props.recipe[i].RCP_PAT2}
+                  </span>
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faFire}
+                      style={{ marginRight: "6px" }}
+                    />
+                    {`${props.recipe[i].INFO_ENG}Kcal`}
+                  </span>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        );
+      })}
+
+      {/* <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav> */}
+    </div>
+  );
+
+}
+
+export default Main;
